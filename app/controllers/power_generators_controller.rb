31f3bcf688ce97fbb4d4d6de29009ea8631c2e74
Murilo Roque <muriloengqui@gmail.com>
2020-07-22 07:52:53 -0300

    implementação de formulário avançado na view

--- app/controllers/power_generators_controller.rb
+++ app/controllers/power_generators_controller.rb
@@ -1,5 +1,6 @@
 class PowerGeneratorsController < ApplicationController
   def index
     @power_generators = PowerGenerator.all
+    @structure_types = PowerGenerator.structure_types.keys
   end
 end
